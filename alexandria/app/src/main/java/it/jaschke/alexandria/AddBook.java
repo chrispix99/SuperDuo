a0cdc2acc9388190ca7c37f2a71e70a598ee9b4d
Chris Pick <chris@munzee.com>
2015-07-13 08:23:56 -0700

    Fix barcode scanner & third issue entering data

--- alexandria/app/src/main/java/it/jaschke/alexandria/AddBook.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/AddBook.java
@@ -76,12 +76,12 @@ public class AddBook extends Fragment implements LoaderManager.LoaderCallbacks<C
                 if(ean.length()==10 && !ean.startsWith("978")){
                     ean="978"+ean;
                 }
-                if(ean.length()<13){
-                    //clearFields();
-                    return;
-                }
                 if (ean.length()==0) {
                     clearFields();
+                    return;
+                }
+                if(ean.length()<13){
+                    //clearFields();
                     return;
                 }
                 //Once we have an ISBN, start a book intent
